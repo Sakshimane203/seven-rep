3722b20fbed756dbb732a3d293bf024089ab1c81
sakshi mane <manesakshi203@gmail.com>
2025-01-20 22:33:17 +0530

    NullPointer.java with initial content

---
+++ NullPointer.java
@@ -0,0 +1,16 @@
+public class NullPointer{
+int x=10;
+public static void main(String[]args){
+try{
+NullPointer a1=null;
+
+System.out.println(a1.x);
+}
+catch(NullPointerException e){
+
+   e.printStackTrace();
+}
+}
+}
+
+ 
